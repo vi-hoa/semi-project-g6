dd28d4c6114c55434e6149e3a835dda18f06c544
Vu Kiet <omgshinn@gmail.com>
2022-12-13 23:15:53 +0700

    gian hang

--- resources/views/admin/listCategory.blade.php
+++ resources/views/admin/listCategory.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('content')
-    <div class="card shadow mb-4">
+    //<div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Category</h6>
         </div>
