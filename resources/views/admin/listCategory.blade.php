e6e4382d56f953d0f12eacf919f469d63cbb8036
Vu Kiet <omgshinn@gmail.com>
2022-12-13 23:21:11 +0700

    Sua lai category

--- resources/views/admin/listCategory.blade.php
+++ resources/views/admin/listCategory.blade.php
@@ -8,7 +8,16 @@
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Category</h6>
         </div>
-       
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Chỉnh sửa</th>
+                        </tr>
                     </thead>
 
                     <tbody>
@@ -25,12 +34,7 @@
                                 <td>{{ $value->description }}</td>
 
                                 <td>
-                                    <a href="{{ asset('category/edit/' . $value->category_id) }}"
-                                        class="btn btn-primary edit"><span class="glyphicon glyphicon-edit"> </span>
-                                        Edit</a>
-                                    <a href="{{ asset('category/delete/' . $value->category_id) }}"
-                                        onclick="return confirm('Bạn có chắc muốn xóa?')" class="btn btn-danger"><span
-                                            class="glyphicon glyphicon-trash"> </span>Delete</a>
+                                    
                                     
                                 </td>
                             </tr>
