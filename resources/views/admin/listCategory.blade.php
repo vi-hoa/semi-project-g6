c22d7ba4160a25215b7367c84a7a000e92f4435c
Vu Kiet <omgshinn@gmail.com>
2022-12-13 23:19:17 +0700

    Change2

--- resources/views/admin/listCategory.blade.php
+++ resources/views/admin/listCategory.blade.php
@@ -4,20 +4,11 @@
 @endsection
 
 @section('content')
-    //<div class="card shadow mb-4">
+    <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Category</h6>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>STT</th>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Chỉnh sửa</th>
-                        </tr>
+       
                     </thead>
 
                     <tbody>
