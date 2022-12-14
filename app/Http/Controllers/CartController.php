041a166aa981b31e2b309dcac097246d2e69edfc
Vu Kiet <omgshinn@gmail.com>
2022-12-14 22:54:03 +0700

    Cap nhat lai model

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -13,59 +13,7 @@ class CartController extends Controller
         if(!$product) {
 
             abort(404);
-        }
-        $cart = session()->get('cart'); 
-        // if cart is empty then this the first product
-        if(!$cart) {
-            $cart = [
-                $id => [
-                    "name" => $product->name,
-                    "quantity" => 1,
-                    "price" => $product->price,
-                    "photo" => $product->image
-                ]
-            ];
-            
-            session()->put('cart', $cart);
-
-            return view('cart');
-
-            //return redirect()->back()->with('success', 'Product added to cart successfully!');
-           
-        }
-        // if cart not empty then check if this product exist then increment quantity 
-        if(isset($cart[$id])) {
-            $cart[$id]['quantity']++;
-            session()->put('cart', $cart);
-            return view('cart', ['msg' => 'product added to cart successfully']);
-            //return redirect()->back()->with('success', 'Product added to cart successfully!');
-        }
-        //if item not exist in cart then add to cart with quantity = 1
-        $cart[$id] = [
-            "name" => $product->name,
-            "quantity" => 1,
-            "price" => $product->price,
-            "photo" => $product->image
-        ];
-
-        session()->put('cart', $cart);
-
-        return view('cart', ['msg' => 'Product added to cart successfully!']);
-        // return redirect()->back()->with('success', 'Product added to cart successfully!');
-        
-    }
-
-    public function remove(Request $request)
-    {
-        if($request-id) {
-            $cart = session()->get('cart');
-
-            if(isset($cart[$request->id])) {
-
-                unset($cart[$request->id]);
-
-                session()->put('cart', $cart);
-            }
+      
 
             return view('cart', ['msg' => 'Delete item in cart successfully!']);
             //session()->flash('success','Product remove successfully');
